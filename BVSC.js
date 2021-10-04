dc2e2a9c4ee2e7b515941f5bf2a486c0fd8d0c84
Syouyu <30584031+SY0UYU@users.noreply.github.com>
2021-10-04 14:38:27 +0800

    Update BVSC.js
    version 0.2.3

--- BVSC.js
+++ BVSC.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Bilibili Video Speed Controler
 // @namespace    https://github.com/SY0UYU/Bilibili-Video-Speed-Controller
-// @version      0.2.1
+// @version      0.2.3
 // @description  加入更多播放倍数选项!
 // @author       SY0UYU
 // @supportURL   https://github.com/SY0UYU/Bilibili-Video-Speed-Controller/issues
@@ -19,20 +19,23 @@
 
     // Your code here...
     var SpeedSelected = undefined;
+    const classActive = 'bilibili-player-active';
+    const classMenu = 'bilibili-player-video-btn-speed-menu';
+    const classItem = 'bilibili-player-video-btn-speed-menu-list';
     const speedList = ['2.5', '3.0', '3.5', '4.0'];
     const rebuildMenu = function (speedMenu) {
-        let elemSource = $('.bilibili-player-video-btn-speed-menu-list')[0];
+        let elemSource = $('.'+classItem)[0];
         for(let speed in speedList){
             let s = speedList[speed]
             let injectElem = $(elemSource).clone(true)[0];
             $(injectElem).attr('data-value',s);
-            $(injectElem).removeClass('bilibili-player-active');
+            $(injectElem).removeClass(classActive);
             $(injectElem).text(s+'x');
             $(speedMenu).prepend(injectElem);
         }
     }
     const injectHTML = function (callback) {
-        let speedMenuList = document.getElementsByClassName('bilibili-player-video-btn-speed-menu');
+        let speedMenuList = document.getElementsByClassName(classMenu);
         if (speedMenuList.length == 0) {
             setTimeout(function () { injectHTML(callback); }, 500);
             return;
@@ -40,24 +43,24 @@
         let speedMenu = speedMenuList[0];
         rebuildMenu(speedMenu);
         $(speedMenu).children().click(function () {
-            $(speedMenu).children().removeClass('bilibili-player-active');
-            $(this).addClass('bilibili-player-active');
+            $(speedMenu).children().removeClass(classActive);
+            $(this).addClass(classActive);
             SpeedSelected = this;
         });
         if (typeof callback == 'function') {
             callback();
         }
-        console.log('Bilibili Video Speed Controler Inject Success! ');
+        console.log('Bilibili Video Speed Controller Inject Success! ');
     };
     const myCallback = function () {
         $('#bilibili-player').click(function () {
-            let list = document.getElementsByClassName('bilibili-player-video-btn-speed-menu-list');
+            let list = document.getElementsByClassName(classItem);
             if (list.length == 6) {
                 injectHTML(function () {
                     if (SpeedSelected != undefined) {
                         let index = $.inArray($(SpeedSelected).attr('data-value'), speedList);
                         if(index != -1){
-                            $('.bilibili-player-video-btn-speed-menu-list[data-value="'+ speedList[index] +'"]').addClass('bilibili-player-active');
+                            $('.'+classItem+'[data-value="'+ speedList[index] +'"]').addClass(classActive);
                         }
                     }
                 });
